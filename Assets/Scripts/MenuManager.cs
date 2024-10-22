c36d727afa541a5d2ffdbadf5298c4b6ce52f424
Karim <karim.muckensturm@viacesi.fr>
2024-10-22 14:03:26 +0200

    import avec file explorer done

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -75,6 +75,34 @@ public class MenuManager : MonoBehaviour
     // Map Modify Panel Buttons //
     public void importMap()
     {
+        // use StandaloneFileBrowser to open a file panel
+        string[] paths = SFB.StandaloneFileBrowser.OpenFilePanel("Open Map", "", "json", false);
+
+        // check if a file was selected
+        if (paths.Length > 0)
+        {
+            // get the file name
+            string fileName = Path.GetFileNameWithoutExtension(paths[0]);
+
+            // check if there is already a file with the same name
+            if (File.Exists(Path.Combine(Application.dataPath, "levels", fileName + ".json")))
+            {
+                Debug.LogError("Map with the same name already exists");
+                return;
+            }
+
+            // copy the file to the levels folder
+            File.Copy(paths[0], Path.Combine(Application.dataPath, "levels", fileName + ".json"));
+
+            // instantiate a new level panel child of gridLevels
+            GameObject levelPanel = Instantiate(levelPanelPrefab, gridLevels.transform);
+
+            // set the level panel name to the level name
+            levelPanel.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = fileName;
+
+            // set the level panel button on click listener
+            levelPanel.GetComponent<Button>().onClick.AddListener(() => loadLevel(levelPanel.GetComponentInChildren<TMPro.TextMeshProUGUI>().text));
+        }
 
     }
     public void backFromModifyPanel()
