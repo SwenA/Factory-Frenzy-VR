36dcb1812d0915ca71d49b432aabe1082b391cf6
Tarkhen <swen.aubertin@viacesi.fr>
2024-10-24 09:39:49 +0200

    commit pre pull main

--- Assets/FactoryFrenzy/Scripts/Item.cs
+++ Assets/FactoryFrenzy/Scripts/Item.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Provider;
 
 public class Item : MonoBehaviour
 {
@@ -13,9 +15,12 @@ public class Item : MonoBehaviour
     XRGrabInteractable grabInteractable;
     private bool isInSlot = true;
     private bool canBeDestroyed = false;
+    private InputDevice device;
+    private bool rotationActivated = false;
     // Start is called before the first frame update
     void Start()
     {
+        device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
         transform.localPosition = itemPos;
         transform.localRotation = itemRot;
         transform.localScale = itemScale;
@@ -27,15 +32,69 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (grabInteractable.isSelected)
+        {
+            bool primary2DAxisClick;
+            Vector2 primary2DAxisValue;
+            device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out primary2DAxisClick);
+            device.TryGetFeatureValue(CommonUsages.primary2DAxis, out primary2DAxisValue);
 
+            if (primary2DAxisClick && !rotationActivated)
+            {
+                // Debug.Log("Primary 2D Axis Click found: " + primary2DAxisClick);
+                // Debug.Log("Primary 2D Axis Click: " + primary2DAxisClick + ", Rotation Activated: " + !rotationActivated);
+                Debug.Log(primary2DAxisValue.x);
+                rotationActivated = true;
+                SmartPlacement(true);
+                if (primary2DAxisValue.x > 0.5f)
+                {
+                    StartCoroutine(RotateItemX(10f));
+                }
+                else if (primary2DAxisValue.x < -0.5f)
+                {
+                    StartCoroutine(RotateItemX(-10f));
+                }
+                if (primary2DAxisValue.y > 0.5f)
+                {
+                    StartCoroutine(RotateItemY(10f));
+                }
+                else if (primary2DAxisValue.y < -0.5f)
+                {
+                    StartCoroutine(RotateItemY(-10f));
+                }
+            }
+            if (!primary2DAxisClick && rotationActivated)
+            {
+                rotationActivated = false;
+            }
+        }
+    }
+
+    IEnumerator RotateItemX(float rotation)
+    {
+        // Debug.Log("Rotating item");
+        
+        transform.Rotate(Vector3.up, rotation);
+        yield return new WaitForSeconds(0.1f);
+        
+    }
+
+    IEnumerator RotateItemY(float rotation)
+    {
+        // Debug.Log("Rotating item");
+        
+        transform.Rotate(Vector3.right, rotation);
+        yield return new WaitForSeconds(0.1f);
+        
     }
 
     public void PickUpItem(SelectEnterEventArgs args)
     {
         GameObject pickedItem = args.interactableObject.transform.gameObject;
+        SmartPlacement(false);
         if (pickedItem.GetComponent<Item>().isInSlot == true)
         {
-            Debug.Log("Picked up item: " + pickedItem.name);
+            // Debug.Log("Picked up item: " + pickedItem.name);
             pickedItem.transform.localScale = Vector3.one;
             pickedItem.GetComponent<Item>().isInSlot = false;
             Instantiate(gameObject, slot, false);
@@ -45,6 +104,7 @@ public class Item : MonoBehaviour
     public void PutDownItem(SelectExitEventArgs args)
     {
         GameObject pickedItem = args.interactableObject.transform.gameObject;
+        grabInteractable.trackPosition = false;
         if (pickedItem != null)
         {
             if(canBeDestroyed)
@@ -53,7 +113,6 @@ public class Item : MonoBehaviour
             }
         }
     }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<InventoryManager>() != null)
@@ -68,5 +127,19 @@ public class Item : MonoBehaviour
         {
             canBeDestroyed = false;
         }
+    }
+
+    void SmartPlacement (bool isActivated)
+    {
+        if (!isActivated)
+        {
+            grabInteractable.trackPosition = true;
+            grabInteractable.trackRotation = true;
+        }
+        else {
+            grabInteractable.trackPosition = false;
+            grabInteractable.trackRotation = false;
+        }
+        Debug.Log("Smart Placement activated: " + isActivated);
     }
 }
