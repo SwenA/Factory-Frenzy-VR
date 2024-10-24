d628653a46963110f98c4370cb42d80c3f417777
Tarkhen <swen.aubertin@viacesi.fr>
2024-10-24 17:41:23 +0200

    item fix

--- Assets/FactoryFrenzy/Scripts/Item.cs
+++ Assets/FactoryFrenzy/Scripts/Item.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -44,6 +43,12 @@ public class Item : MonoBehaviour
         grabInteractable.selectEntered.AddListener(PickUpItem);
         grabInteractable.selectExited.AddListener(PutDownItem);
         grabInteractable.activated.AddListener(SmartPlacement);
+        grabInteractable.activated.AddListener(lockItem);
+
+        audioSourceGrab = gameObject.AddComponent<AudioSource>();
+        audioSourceGrab.clip = Resources.Load<AudioClip>("grab");
+        audioSourceDrop = gameObject.AddComponent<AudioSource>();
+        audioSourceDrop.clip = Resources.Load<AudioClip>("drop");
     }
 
     // Update is called once per frame
@@ -114,6 +119,130 @@ public class Item : MonoBehaviour
         
     }
 
+    public void lockItem(ActivateEventArgs args)
+    {
+        // Check if the activating controller is the left hand
+        if (args.interactorObject.transform.parent.name == "Left Controller")
+        {
+            Debug.Log("Locked item: " + gameObject.name);
+            isLocked = !isLocked;
+
+            if (isLocked)
+            {
+                grabInteractable.trackPosition = false;
+                grabInteractable.trackRotation = false;
+                SetGlobalOpacity(0.7f);
+            }
+            else
+            {
+                grabInteractable.trackPosition = true;
+                grabInteractable.trackRotation = true;
+                SetGlobalOpacity(1.0f);
+            }
+        }
+    }
+
+    #region Design
+    // Fonction qui ajuste l'opacité de tous les matériaux d'un parent et de ses enfants
+    void SetGlobalOpacity(float opacity)
+    {
+        // Récupère tous les renderers de l'objet parent et de ses enfants
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
+        {
+            // Pour chaque renderer, récupère tous les matériaux
+            foreach (Material mat in renderer.materials)
+            {
+                if (opacity < 1.0f)
+                {
+                    // Change le matériau en transparent
+                    SetMaterialToTransparent(mat);
+                }
+                else
+                {
+                    // Change le matériau en opaque
+                    SetMaterialToOpaque(mat);
+                }
+
+                // Vérifie si le shader du matériau possède la propriété "_BaseColor" (ou "_Color")
+                if (mat.HasProperty("_BaseColor") || mat.HasProperty("_Color"))
+                {
+                    // Récupère la couleur actuelle du matériau
+                    Color baseColor = mat.HasProperty("_BaseColor")
+                        ? mat.GetColor("_BaseColor")
+                        : mat.GetColor("_Color");
+
+                    // Modifie l'alpha (opacité)
+                    baseColor.a = opacity;
+
+                    // Applique la nouvelle couleur avec l'alpha modifié
+                    if (mat.HasProperty("_BaseColor"))
+                    {
+                        mat.SetColor("_BaseColor", baseColor);
+                    }
+                    else if (mat.HasProperty("_Color"))
+                    {
+                        mat.SetColor("_Color", baseColor);
+                    }
+                }
+            }
+        }
+    }
+
+    // Fonction pour changer le mode de rendu du matériau en transparent
+    void SetMaterialToTransparent(Material mat)
+    {
+        // Change le mode de rendu en Transparent avec Alpha Blending
+        mat.SetFloat("_Surface", 1); // Surface Type : Transparent
+        mat.SetFloat("_Blend", 0); // Blending Mode : Alpha
+        mat.SetFloat("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        mat.SetFloat("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        mat.SetFloat("_ZWrite", 0); // Désactiver l'écriture dans le Z-buffer
+        mat.SetFloat("_AlphaClip", 0); // Alpha Clipping désactivé
+
+        mat.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
+        mat.EnableKeyword("_BLENDMODE_ALPHA");
+        mat.DisableKeyword("_ALPHATEST_ON"); // Désactiver l'alpha test
+        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON"); // Désactiver le mode Alpha premultiplied
+
+        // En fonction de tes paramètres, la face de rendu est "Front" seulement
+        mat.SetFloat("_Cull", (int)UnityEngine.Rendering.CullMode.Back);
+
+        // Configurer la file d'attente de rendu pour transparent
+        mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+    }
+
+    // Fonction pour changer le mode de rendu du matériau en opaque
+    void SetMaterialToOpaque(Material mat)
+    {
+        // Rétablir le mode de rendu en opaque
+        mat.SetFloat("_Surface", 0); // Surface Type : Opaque
+        mat.SetFloat("_Blend", 0); // Blending Mode : Opaque
+        mat.SetFloat("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One); // Source blend
+        mat.SetFloat("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero); // Destination blend
+        mat.SetFloat("_ZWrite", 1); // Activer l'écriture dans le Z-buffer
+        mat.SetFloat("_AlphaClip", 1); // Alpha Clipping activé
+
+        mat.DisableKeyword("_SURFACE_TYPE_TRANSPARENT");
+        mat.EnableKeyword("_ALPHATEST_ON"); // Activer le test alpha
+        mat.DisableKeyword("_BLENDMODE_ALPHA"); // Désactiver le blending alpha
+        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON"); // Désactiver le mode Alpha premultiplied
+
+        // Rendre seulement la face avant (Cull Front)
+        mat.SetFloat("_Cull", (int)UnityEngine.Rendering.CullMode.Back); // Garder "Front"
+
+        // File d'attente de rendu pour opaque
+        mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
+
+        // Paramètres additionnels selon ton image
+        mat.SetFloat("_ReceiveShadows", 1); // Activer les ombres
+    }
+
+    #endregion
+
+    
+
     public void PickUpItem(SelectEnterEventArgs args)
     {
         GameObject pickedItem = args.interactableObject.transform.gameObject;
@@ -159,19 +288,22 @@ public class Item : MonoBehaviour
 
     void SmartPlacement (ActivateEventArgs args)
     {
-        Vector3 itemRotation = transform.rotation.eulerAngles;
-        itemRotation.x = Mathf.Round(itemRotation.x / 10) * 10;
-        itemRotation.y = Mathf.Round(itemRotation.y / 10) * 10;
-        itemRotation.z = Mathf.Round(itemRotation.z / 10) * 10;
-        transform.rotation = Quaternion.Euler(itemRotation);
-        smartPlacementActivated = !smartPlacementActivated;
-        if (!smartPlacementActivated)
+        if (args.interactorObject.transform.parent.name == "right Controller")
         {
-            grabInteractable.trackRotation = true;
+            Vector3 itemRotation = transform.rotation.eulerAngles;
+            itemRotation.x = Mathf.Round(itemRotation.x / 10) * 10;
+            itemRotation.y = Mathf.Round(itemRotation.y / 10) * 10;
+            itemRotation.z = Mathf.Round(itemRotation.z / 10) * 10;
+            transform.rotation = Quaternion.Euler(itemRotation);
+            smartPlacementActivated = !smartPlacementActivated;
+            if (!smartPlacementActivated)
+            {
+                grabInteractable.trackRotation = true;
+            }
+            else {
+                grabInteractable.trackRotation = false;
+            }
+            Debug.Log("Smart Placement activated: " + smartPlacementActivated);
         }
-        else {
-            grabInteractable.trackRotation = false;
-        }
-        Debug.Log("Smart Placement activated: " + smartPlacementActivated);
     }
 }
